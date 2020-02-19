2526d38aed3183e0217a4023c06d38760e69a938
James <james_gower@outlook.com>
2020-02-19 21:04:38 +0000

    styled login component, added option to reset lost password

--- src/pages/accounts/components/Login.tsx
+++ src/pages/accounts/components/Login.tsx
@@ -1,21 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Col, Row } from "reactstrap";
 import { Auth } from "aws-amplify";
 import { CognitoHostedUIIdentityProvider as Provider } from "@aws-amplify/auth/lib/types";
 import { ICredentials } from "@aws-amplify/core";
-import {
-  FormGroup,
-  InputGroup,
-  Button,
-  H4,
-  Overlay,
-  H3,
-  Dialog,
-} from "@blueprintjs/core";
+import { FormGroup, InputGroup, Button, H4, Dialog } from "@blueprintjs/core";
+import { withRouter } from "react-router-dom";
 import { Toaster } from "../../../utils";
-import { Authenticator } from "aws-amplify-react";
 
 interface Props {}
+
+/**
+ * TODO
+ * [ ] Add create account
+ * [ ] Add option to show password on click
+ */
 
 class Login extends React.Component {
   public readonly state = {
@@ -23,13 +21,17 @@ class Login extends React.Component {
     password: "",
     codeInput: false,
     dialogOpen: false,
+    code: "",
+    newPassword: "",
+    codeAddress: null,
   };
 
   private handleSignIn = async (): Promise<void> => {
     const { username, password } = this.state;
+    const { history } = this.props;
     try {
-      const res = await Auth.signIn(username, password);
-      console.log(res);
+      await Auth.signIn(username, password);
+      history.push("/");
     } catch (err) {
       console.error(err);
       Toaster.show({
@@ -39,21 +41,53 @@ class Login extends React.Component {
     }
   };
 
-  public readonly handleForgottenPassword = async (): Promise<void> => {
-    const { username, password } = this.state;
+  private handleForgottenPassword = async (): Promise<void> => {
+    const { username } = this.state;
     try {
-      await Auth.forgotPassword(username);
-      this.setState({ codeInput: true });
+      const res = await Auth.forgotPassword(username);
+      this.setState({
+        codeInput: true,
+        codeAddress: res.CodeDeliveryDetails.Destination,
+      });
     } catch (err) {
+      console.error(err);
       Toaster.show({
         intent: "danger",
-        message: "User not found. Please check your username.",
+        message:
+          err.name === "LimitExceededException"
+            ? "Attempt limit succeeded - Please try again later."
+            : "User not found. Please check your username.",
+      });
+    }
+  };
+
+  private handleCodeVerify = async (): Promise<void> => {
+    const { username, code, newPassword } = this.state;
+    try {
+      await Auth.forgotPasswordSubmit(username, code, newPassword);
+      Toaster.show({
+        intent: "success",
+        message: "Successfully changed password.",
+      });
+    } catch (err) {
+      console.error(err);
+      Toaster.show({
+        intent: "danger",
+        message: err.message || "Error updating password",
       });
     }
   };
 
   public render(): JSX.Element {
-    const { username, password, dialogOpen } = this.state;
+    const {
+      username,
+      password,
+      dialogOpen,
+      codeInput,
+      code,
+      newPassword,
+      codeAddress,
+    } = this.state;
     return (
       <>
         <Container>
@@ -99,41 +133,49 @@ class Login extends React.Component {
               </div>
             </Col>
             <Col sm={6}>
-              <div className="login__cognito">
+              <div className="login__cognito-container">
                 <div className="login__text">
                   <H4>Login with your account</H4>
                   <p>Please login with your user credentials, or create a new account.</p>
                 </div>
-                <FormGroup label="Username">
-                  <InputGroup
-                    value={username}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-                      this.setState({ username: e.target.value })
+                <div className="login__cognito">
+                  <FormGroup label="Username">
+                    <InputGroup
+                      value={username}
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                        this.setState({ username: e.target.value })
+                      }
+                    />
+                  </FormGroup>
+                  <FormGroup
+                    label="Password"
+                    helperText={
+                      <div
+                        className="login__forgot"
+                        tabIndex={0}
+                        role="button"
+                        onClick={(): void => this.setState({ dialogOpen: true })}
+                      >
+                        Forgot your password?
+                      </div>
                     }
-                  />
-                </FormGroup>
-                <FormGroup
-                  label="Password"
-                  helperText={
-                    <div
-                      className="login__forgot"
-                      tabIndex={0}
-                      role="button"
-                      onClick={(): void => this.setState({ dialogOpen: true })}
-                    >
-                      Forgot your password?
-                    </div>
-                  }
-                >
-                  <InputGroup
-                    value={password}
-                    type="password"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-                      this.setState({ password: e.target.value })
-                    }
-                  />
-                </FormGroup>
-                <Button onClick={(): Promise<void> => this.handleSignIn()}>Login</Button>
+                  >
+                    <InputGroup
+                      value={password}
+                      type="password"
+                      onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                        this.setState({ password: e.target.value })
+                      }
+                    />
+                  </FormGroup>
+                  <Button
+                    intent="success"
+                    onClick={(): Promise<void> => this.handleSignIn()}
+                  >
+                    Login
+                  </Button>
+                  <p className="login__create">Not got an account? Create one here.</p>
+                </div>
               </div>
             </Col>
           </Row>
@@ -145,25 +187,60 @@ class Login extends React.Component {
           className="login__dialog-container"
         >
           <div className="login__dialog">
-            <p>
-              Please enter the username associated with your account. You will be emailed
-              a code which will let you change your password.
-            </p>
-            <FormGroup label="Username">
-              <InputGroup
-                value={username}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-                  this.setState({ username: e.target.value })
-                }
-              />
-            </FormGroup>
-            <Button
-              intent="success"
-              onClick={(): Promise<void> => this.handleForgottenPassword()}
-              className="login__dialog-button"
-            >
-              Send Code
-            </Button>
+            {codeInput ? (
+              <>
+                <p>
+                  A code has been sent to {codeAddress}. Please enter it into the input
+                  below and create a new password.
+                </p>
+                <FormGroup label="Code">
+                  <InputGroup
+                    value={code}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                      this.setState({ code: e.target.value })
+                    }
+                  />
+                </FormGroup>
+                <FormGroup label="New Password">
+                  <InputGroup
+                    value={newPassword}
+                    type="password"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                      this.setState({ newPassword: e.target.value })
+                    }
+                  />
+                </FormGroup>
+                <Button
+                  intent="success"
+                  onClick={this.handleCodeVerify}
+                  className="login__dialog-button"
+                >
+                  Update Password
+                </Button>
+              </>
+            ) : (
+              <>
+                <p>
+                  Please enter the username associated with your account. You will be
+                  emailed a code which will let you change your password.
+                </p>
+                <FormGroup label="Username">
+                  <InputGroup
+                    value={username}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                      this.setState({ username: e.target.value })
+                    }
+                  />
+                </FormGroup>
+                <Button
+                  intent="success"
+                  onClick={this.handleForgottenPassword}
+                  className="login__dialog-button"
+                >
+                  Send Code
+                </Button>
+              </>
+            )}
           </div>
         </Dialog>
       </>
@@ -171,4 +248,4 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+export default withRouter(Login);
